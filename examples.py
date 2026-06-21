b249067c10003f58adf84e4c4e71bd0d7753698b
Gülce Candemir <gulcecandemir2011@gmail.com>
2026-06-21 14:58:22 +0300

    feat: add session 1 assets and scripts

---
+++ examples.py
@@ -0,0 +1,9 @@
+#Açık (Explicit - İyi): kullanici_olustur(
+#isim="Ahmet", yas=25, premium_mu=True)
+
+# GENEL KURAL: Her kullanıcı verisi kaydedilmeden önce doğrulanır.
+'''def kaydet_kullanici_profili(kullanici_verisi):
+    if dogrula(kullanici_verisi):
+        veritabanina_yaz(kullanici_verisi)
+    else:
+        raise ValueError("Geçersiz veri!")'''
---
+++ importTHIS.py
@@ -0,0 +1,45 @@
+import this
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+''''
+Güzel çirkinden iyidir. (Beautiful is better than ugly.)
+Tahmin ettirme, açık yaz. (Explicit is better than implicit.)
+Basit karmaşıktan iyidir. (Simple is better than complex.)
+Karmaşık karışıktan iyidir. (Complex is better than complicated.)
+Okunabilirlik önemlidir. (Readability counts.)
+Hatalar asla sessizce geçiştirilmemelidir. (Errors should never pass silently.)
+Özel durumlar kuralları bozacak kadar özel değildir. (Special cases aren't special enough to break rules.)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
